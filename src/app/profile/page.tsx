f9777b1399c77b17bca411ea276425991a62c380
Ashish Chaudhary <chaudharyaashish002@gmail.com>
2025-12-19 15:12:41 +0545

    dynamic routing and typescript

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+
+import Link from "next/link"
+import { useState } from "react"
+import { defUsers } from "../constants/constants"
+
+interface IUSER {
+  id: string
+  username: string
+  email: string
+  address: string
+  facebook: string
+  image: string
+}
+
+const Page = () => {
+  const [users] = useState<IUSER[]>(defUsers)
+
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      {users.map((user) => (
+        <Link
+          key={user.id}
+          href={`/profile/${user.id}`}
+          className="border p-4 rounded-sm"
+        >
+          <div>{user.id}</div>
+          <div>{user.username}</div>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Page
